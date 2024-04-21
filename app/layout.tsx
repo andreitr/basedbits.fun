2ad07f29a55db3ebf9fc310c9a39e0964a94b9a4
andreitr <andrei.taraschuk@gmail.com>
2024-04-21 07:12:01 -0600

    Home layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={roboto.className}>{children}</body>
+        <body className="font-mono">{children}</body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col p-24">
-            <div className="text-5xl font-medium text-[#394039]">
-                Based Bits
+        <main className="flex justify-center mt-20 gap-10">
+            <div className="flex flex-col items-center">
+                <Image src={'/images/developer.png'} alt={'based bits'} width={200} height={200}/>
             </div>
-            <div className="font-xs flex text-thin flex-row gap-2 mt-2 text-[#829082]">
-                <a href="https://rarible.com/BasedBits">Rarible</a>
-                <a href="https://opensea.io/collection/based-bits">OpenSea</a>
-                <a href="https://warpcast.com/basedbits">Farcaster</a>
-                <a href="https://twitter.com/basedbits_fun">Twitter</a>
+            <div className="flex items-center justify-center">
+                <div className="flex-col bg-white p-5">
+                    <div className="mb-2">Based Bits</div>
+                    <div className="text-sm">
+                        <Link href="https://opensea.io/collection/based-bits" target="_blank">
+                            https://opensea.io/collection/based-bits
+                        </Link>
+                    </div>
+                    <div className="text-sm">
+                        <Link href="https://rarible.com/BasedBits" target="_blank">
+                            https://rarible.com/BasedBits
+                        </Link>
+                    </div>
+                    <div className="text-sm">
+                        <Link href="https://warpcast.com/basedbits" target="_blank">
+                            https://warpcast.com/basedbits
+                        </Link>
+                    </div>
+                    <div className="text-sm">
+                        <Link href="https://x.com/basedbits_fun" target="_blank">
+                            https://x.com/basedbits_fun
+                        </Link>
+                    </div>
+                </div>
+                                
             </div>
         </main>
     );
