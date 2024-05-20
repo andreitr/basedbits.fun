800a09279502471e3321b78b6952fa92ffee1969
andrei <andrei.taraschuk@gmail.com>
2024-05-19 19:56:41 -0600

    API route for badges metadata.

---
+++ app/api/badges/1/route.ts
@@ -0,0 +1,18 @@
+import {NextResponse} from "next/server";
+
+export async function GET(request: Request) {
+
+    return NextResponse.json(
+        {
+            name: "7 Day Streak",
+            description: "This badge is awarded for checking into basedbits.fun for 7 days in a row.",
+            image: "https://ipfs.raribleuserdata.com/ipfs/QmRqqnZsbMLJGWt8SWjP2ebtzeHtWv5kkz3brbLzY1ShHt/5709.png",
+            tokenId: "1",
+            attributes: [
+                {
+                    "trait_type": "artist",
+                    "value": "numo.eth"
+                },
+            ]
+        });
+};
