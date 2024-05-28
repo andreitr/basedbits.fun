192710194988355ab0ac9340831984ba0072e2ab
andrei <andrei.taraschuk@gmail.com>
2024-05-28 17:14:17 -0600

    Work in progress

--- app/badges/[id]/components/BadgeDetails.tsx
+++ app/badges/[id]/components/BadgeDetails.tsx
@@ -9,8 +9,8 @@ interface BadgeProps {
 export const BadgeDetails = ({id}: BadgeProps) => {
     return <div className="flex flex-col justify-between mt-8 sm:flex-row gap-8">
         <Image
-            className="w-auto max-w-72 m-auto sm:m-0"
-            src="/images/developer.png"
+            className="w-auto max-w-72 m-auto sm:m-0 grayscale opacity-80 hover:opacity-100 hover:grayscale-0 transition duration-300 ease-in-out"
+            src="/images/badges/003.png"
             alt="Are you here?"
             width={250}
             height={250}
@@ -19,11 +19,13 @@ export const BadgeDetails = ({id}: BadgeProps) => {
         <div className="flex flex-col justify-center mt-8 sm:mt-0 sm:ml-4">
 
             <div className="text-6xl font-semibold text-[#363E36] mb-4">
-                First Click
+                First Click OG
             </div>
             <div className="text-[#677467]">
-                <p>This badge can only be minted by the brave souls who checked into Based Bits during the fist week. </p>
+                <p>This badge can only be minted by the brave souls who checked into Based Bits during the fist
+                    week. </p>
             </div>
+            <div className="mt-8">Login to mint!</div>
         </div>
 
     </div>
