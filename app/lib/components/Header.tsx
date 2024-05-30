418c70b4c61ca2c22ee13c560ceb29d61ef7caa2
andrei <andrei.taraschuk@gmail.com>
2024-05-29 20:02:26 -0600

    Favicon and noggles

--- app/lib/components/Header.tsx
+++ app/lib/components/Header.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import { ConnectKitButton } from "connectkit";
+import {ConnectKitButton} from "connectkit";
+import Image from "next/image";
 
 export const Header = () => {
-  return (
-    <div className="flex flex-row py-6 text-right justify-between">
-      <div className="bg-green-400 rounded-lg px-6 py-2 bg-[#c4dac4]">⌐⊡-⊡</div>
-      <ConnectKitButton />
-    </div>
-  );
+    return (
+        <div className="flex flex-row py-6 text-right justify-between">
+            <div className="bg-[#ABBEAC] px-5 py-2 rounded-lg">
+                <Image
+                    src="/images/noggles.png"
+                    alt="Nerd Noggles"
+                    width={65}
+                    height={90}
+                    priority={true}
+                />
+            </div>
+
+            <ConnectKitButton/>
+        </div>
+    );
 };
