b53bed46023d47ad75df09a8da13d9147f53a416
andrei <andrei.taraschuk@gmail.com>
2024-07-25 10:38:39 -0600

    Token changes

---
+++ app/lib/api/getUserTokenBalance.ts
@@ -0,0 +1,14 @@
+import {readContract} from "@wagmi/core";
+import {baseConfig} from "@/app/lib/Web3Configs";
+import {BBitsTokenAbi} from "@/app/lib/abi/BBitsToken.abi";
+import {BigNumberish} from "ethers";
+
+export async function getUserTokenBalance(address: `0x${string}`) {
+    const data: any = await readContract(baseConfig, {
+        abi: BBitsTokenAbi,
+        address: process.env.NEXT_PUBLIC_BB_TOKEN_ADDRESS as `0x${string}`,
+        functionName: "balanceOf",
+        args: [address],
+    });
+    return data as BigNumberish;
+}
