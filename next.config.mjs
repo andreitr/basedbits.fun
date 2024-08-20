0d2401b207db683b057d1c74ace632617e202e0a
andrei <andrei.taraschuk@gmail.com>
2024-08-20 10:49:22 -0600

    Header config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,38 @@ const nextConfig = {
     images: {
         domains: ['ipfs.raribleuserdata.com', 'res.cloudinary.com'],
     },
+
+    async headers() {
+        return [
+            {
+                source: "/api/images/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "max-age=1800",
+                    },
+                ],
+            },
+            {
+                source: "/raffle/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "max-age=1800",
+                    },
+                ],
+            },
+            {
+                source: "/users/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "max-age=1800",
+                    },
+                ],
+            },
+        ]
+    }
 };
 
 export default nextConfig;
