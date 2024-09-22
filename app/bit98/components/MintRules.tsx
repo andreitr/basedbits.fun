4707ea29852d393401627e845b6ab1dcd7033bd9
andrei <andrei.taraschuk@gmail.com>
2024-09-22 12:41:57 -0600

    Misc

--- app/bit98/components/MintRules.tsx
+++ app/bit98/components/MintRules.tsx
@@ -3,7 +3,7 @@ export const MintRules = () => {
     <div className="flex flex-col gap-2 p-4 bg-black bg-opacity-10 rounded-lg">
       <li>All minters are entered into a raffle for a 1:1 NFT.</li>
       <li>A raffled NFT is generated and sent at the end of the mint.</li>
-      <li>The more Bit98's you hold, the more raffle entries you receive.</li>
+      <li>The more Bit98s you hold, the more raffle entries you receive.</li>
       <li>Starting a new mint grants you a free NFT.</li>
       <li>
         Check-in streaks = mint discounts. 1 day = 1% off (up to a max of 90%)
