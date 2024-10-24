300ec06ba139384084f1dc4accdd3df9fc45bb2a
andrei <andrei.taraschuk@gmail.com>
2024-10-24 16:26:57 -0600

    Cron test

--- app/api/bots/burner/route.ts
+++ app/api/bots/burner/route.ts
@@ -1,16 +1,7 @@
-import { ethers, JsonRpcProvider, parseUnits, Wallet } from "ethers";
+export const dynamic = "force-dynamic";
+
+import { Contract, JsonRpcProvider, parseUnits, Wallet } from "ethers";
 import { BBitsBurnerAbi } from "@/app/lib/abi/BBitsBurner.abi";
-
-const provider = new JsonRpcProvider(
-  `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
-);
-const signer = new Wallet(
-  process.env.NEXT_PUBLIC_BURNER_BOT_PK as string,
-  provider,
-);
-
-const contractAddress = process.env.NEXT_PUBLIC_BB_BURNER_ADDRESS as string;
-const contract = new ethers.Contract(contractAddress, BBitsBurnerAbi, signer);
 
 export async function GET(req: Request) {
   try {
@@ -21,13 +12,27 @@ export async function GET(req: Request) {
       });
     }
 
-    const minAmountToBurn = parseUnits("0.0004", 18);
+    const provider = new JsonRpcProvider(
+      `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+    );
 
-    contract.burn(minAmountToBurn, {
-      value: minAmountToBurn,
+    const signer = new Wallet(
+      process.env.NEXT_PUBLIC_BURNER_BOT_PK as string,
+      provider,
+    );
+
+    const contract = new Contract(
+      process.env.NEXT_PUBLIC_BB_BURNER_ADDRESS as string,
+      BBitsBurnerAbi,
+      signer,
+    );
+    const burnAmount = parseUnits("0.0004", 18);
+
+    contract.burn(burnAmount, {
+      value: burnAmount,
     });
 
-    return new Response(JSON.stringify({ status: "burned" }), {
+    return new Response("Burned", {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
