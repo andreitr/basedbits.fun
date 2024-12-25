a4fc3c3e08bfaea8930edc247fe0b3bd95cc4954
andrei <andrei.taraschuk@gmail.com>
2024-12-25 14:49:51 -0700

    Checkin airdrop

--- app/api/bots/airdropper/route.ts
+++ app/api/bots/airdropper/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from "next/server";
 import { Contract, JsonRpcProvider, parseUnits, Wallet } from "ethers";
-import { BBitsBurnerAbi } from "@/app/lib/abi/BBitsBurner.abi";
+import { getRecentCheckIns } from "@/app/lib/api/getRecentCheckIns";
+import { BBitsTokenAbi } from "@/app/lib/abi/BBitsToken.abi";
 
 export async function GET(req: NextRequest) {
   try {
@@ -10,25 +11,33 @@ export async function GET(req: NextRequest) {
         status: 401,
       });
     }
+
+    const checkins = await getRecentCheckIns();
+
+    if (checkins.length === 0) {
+      return new Response("No recent check-ins found", { status: 200 });
+    }
+
+    const reward = 100 / checkins.length;
+    const rewardAmount = parseUnits(reward.toString(), 18);
+
     const provider = new JsonRpcProvider(
       `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
     );
 
-    const signer = new Wallet(process.env.BURNER_BOT_PK as string, provider);
+    const signer = new Wallet(process.env.AIRDROP_BOT_PK as string, provider);
 
     const contract = new Contract(
-      process.env.NEXT_PUBLIC_BB_BURNER_ADDRESS as string,
-      BBitsBurnerAbi,
+      process.env.NEXT_PUBLIC_BB_TOKEN_ADDRESS as string,
+      BBitsTokenAbi,
       signer,
     );
 
-    const burnAmount = parseUnits("0.0004", 18);
+    for (const checkin of checkins) {
+      await contract.transfer(checkin, rewardAmount);
+    }
 
-    await contract.burn(burnAmount, {
-      value: burnAmount,
-    });
-
-    return new Response("Burned", {
+    return new Response("Daily airdrop sent", {
       status: 200,
     });
   } catch (error) {
