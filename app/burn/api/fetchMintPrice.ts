e6ef197c7365f59a9ade7204358f01fd29ccf086
andrei <andrei.taraschuk@gmail.com>
2024-12-27 16:36:14 -0700

    Fixed static calls

--- app/burn/api/fetchMintPrice.ts
+++ app/burn/api/fetchMintPrice.ts
@@ -1,17 +1,18 @@
-import { Contract } from "ethers";
-import { BurnedBitsABI } from "@/app/lib/abi/BurnedBits.abi";
-import { baseProvider } from "@/app/lib/Web3Configs";
+import {Contract} from "ethers";
+import {BurnedBitsABI} from "@/app/lib/abi/BurnedBits.abi";
+import {baseProvider} from "@/app/lib/Web3Configs";
 
 const minter = new Contract(
-  process.env.NEXT_PUBLIC_BURNED_BITS_ADDRESS as `0x${string}`,
-  BurnedBitsABI,
-  baseProvider,
+    process.env.NEXT_PUBLIC_BURNED_BITS_ADDRESS as `0x${string}`,
+    BurnedBitsABI,
+    baseProvider,
 );
 
 export const fetchMintPrice = async () => {
-  try {
-    return await minter.mintPriceInWETH.staticCall();
-  } catch (error) {
-    console.error("Error fetching price:", error);
-  }
+    try {
+        const mintPriceFn = minter.getFunction("mintPriceInWETH");
+        return await mintPriceFn.staticCall();
+    } catch (error) {
+        console.error("Error fetching price:", error);
+    }
 };
--- app/lib/utils/uniswap.ts
+++ app/lib/utils/uniswap.ts
@@ -1,30 +1,27 @@
-import { Contract, parseUnits } from "ethers";
+import {Contract, parseUnits} from "ethers";
 import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json";
-import { QUOTER_ADDRESSES } from "@uniswap/sdk-core";
-import { base } from "wagmi/chains";
-import { baseProvider } from "@/app/lib/Web3Configs";
+import {baseProvider} from "@/app/lib/Web3Configs";
 
 const quoterContract = new Contract(
-  "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
-  Quoter.abi,
-  baseProvider,
+    "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
+    Quoter.abi,
+    baseProvider,
 );
 
-console.log(QUOTER_ADDRESSES[base.id]);
-
 export const fetchTokenPrice = async () => {
-  try {
-    const params = {
-      tokenIn: "0x553C1f87C2EF99CcA23b8A7fFaA629C8c2D27666",
-      tokenOut: "0x4200000000000000000000000000000000000006",
-      amountIn: parseUnits("1024", 18),
-      fee: BigInt("3000"),
-      sqrtPriceLimitX96: 0,
-    };
-    const amount =
-      await quoterContract.quoteExactInputSingle.staticCall(params);
-    return amount[0].toString();
-  } catch (error) {
-    console.error("Error fetching price:", error);
-  }
+    try {
+
+        const fn = quoterContract.getFunction("quoteExactInputSingle");
+        const [amountOut] = await fn.staticCall({
+            tokenIn: "0x553C1f87C2EF99CcA23b8A7fFaA629C8c2D27666",
+            tokenOut: "0x4200000000000000000000000000000000000006",
+            fee: BigInt('3000'),
+            amountIn: parseUnits("1024", 18),
+            sqrtPriceLimitX96: BigInt(0),
+        });
+
+        return amountOut.toString();
+    } catch (error) {
+        console.error("Error fetching price:", error);
+    }
 };
