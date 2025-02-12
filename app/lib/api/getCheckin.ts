2087285b337d97425ffa1624090121da68b7a29c
andrei <andrei.taraschuk@gmail.com>
2025-02-12 13:29:00 -0700

    Replaced cron with checkin hydrate

--- app/lib/api/getCheckin.ts
+++ app/lib/api/getCheckin.ts
@@ -1,24 +1,24 @@
-import { readContract } from "@wagmi/core";
-import { baseConfig } from "@/app/lib/Web3Configs";
-import { CheckIn } from "@/app/lib/types/types";
-import { BBitsCheckInABI } from "@/app/lib/abi/BBitsCheckIn.abi";
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
+import {readContract} from "@wagmi/core";
+import {baseConfig} from "@/app/lib/Web3Configs";
+import {CheckIn} from "@/app/lib/types/types";
+import {BBitsCheckInABI} from "@/app/lib/abi/BBitsCheckIn.abi";
+import {unstable_cache} from "next/cache";
+import {cache} from "react";
 
 // Read contract data
 export const fetchCheckin = async (address: string) => {
-  const data: any = await readContract(baseConfig, {
-    abi: BBitsCheckInABI,
-    address: process.env.NEXT_PUBLIC_BB_CHECKINS_ADDRESS as `0x${string}`,
-    functionName: "checkIns",
-    args: [address],
-  });
+    const data: any = await readContract(baseConfig, {
+        abi: BBitsCheckInABI,
+        address: process.env.NEXT_PUBLIC_BB_CHECKINS_ADDRESS as `0x${string}`,
+        functionName: "checkIns",
+        args: [address],
+    });
 
-  return {
-    lastCheckin: Number(data[0].toString()),
-    streak: data[1],
-    count: data[2],
-  } as CheckIn;
+    return {
+        lastCheckin: Number(data[0].toString()),
+        streak: data[1],
+        count: data[2],
+    } as CheckIn;
 };
 
 // Avoid duplicate concurrent calls by wrapping the function in a cache
@@ -26,7 +26,7 @@ const cachedFetchCheckin = cache(fetchCheckin);
 
 // Cache results on the server
 export const getCheckin = (address: string) =>
-  unstable_cache(() => cachedFetchCheckin(address), [`checkIns-${address}`], {
-    tags: [`checkIns-${address}`],
-    revalidate: 86_400, //24 hours
-  })();
+    unstable_cache(() => cachedFetchCheckin(address), [`checkIns-${address}`], {
+        tags: [`checkIns-${address}`],
+        revalidate: 86_400, //24 hours
+    })();
---
+++ app/lib/hooks/useHydrateUser.ts
@@ -0,0 +1,14 @@
+import {useCallback} from "react";
+
+export const useHydrateUser = () => {
+
+    const call = useCallback(async (address: string) => {
+        try {
+            await fetch(`/api/hydrate/users/${address}`);
+        } catch (error) {
+            console.error("Failed to revalidate", error);
+        }
+    }, []);
+
+    return {call};
+};
