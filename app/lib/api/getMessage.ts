917ddd86612de4795d9ded46f0b00b48b1932321
andrei <andrei.taraschuk@gmail.com>
2025-03-31 11:47:33 -0600

    Retrieve message api

---
+++ app/lib/api/getMessage.ts
@@ -0,0 +1,20 @@
+import { supabase } from "@/app/lib/supabase/client";
+import { DBMessage } from "@/app/lib/types/types";
+import { cache } from "react";
+
+export const fetchMessageDB = async (hash: string): Promise<DBMessage | null> => {
+    const { data, error } = await supabase
+        .from("messages")
+        .select("*")
+        .eq("message_hash", hash)
+        .single();
+
+    if (error) {
+        console.error("Error fetching message:", error);
+        return null;
+    }
+
+    return data as DBMessage;
+};
+
+export const getMessageDB = cache(fetchMessageDB); 
