852b3a19f42bdc88b8a3cb3d79be16ff5504c4cb
andrei <andrei.taraschuk@gmail.com>
2025-04-11 17:18:46 -0600

    Hotfix

--- app/api/bots/zeitgeist/compose/route.ts
+++ app/api/bots/zeitgeist/compose/route.ts
@@ -11,10 +11,10 @@ const openai = new OpenAI({
 
 export async function GET(req: NextRequest) {
   try {
-    // const authHeader = req.headers.get("authorization");
-    // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    //   return new Response("Unauthorized", { status: 401 });
-    // }
+    const authHeader = req.headers.get("authorization");
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return new Response("Unauthorized", { status: 401 });
+    }
 
     const allWords = await supabase
       .from("zeitgeist")
