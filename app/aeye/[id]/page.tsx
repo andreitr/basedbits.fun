390722ed685210d55d59d611772ba375c398c816
andrei <andrei@andreis-MacBook-Pro-2.local>
2025-05-18 07:46:13 -0600

    Implemented token specific page

---
+++ app/aeye/[id]/page.tsx
@@ -0,0 +1,36 @@
+"use server";
+
+import { MintComponent } from "@/app/aeye/components/MintComponent";
+import { Header } from "@/app/lib/components/client/Header";
+import { Footer } from "@/app/lib/components/Footer";
+import { getTokenMetadata } from "../../lib/api/aeye/getTokenMetadata";
+import { AeyeTokenMetadata } from "../../lib/api/aeye/getTokenMetadata";
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const title = `AEYE #${params.id}`;
+  const description = "AEYE records the rise of artificial intelligence by minting a single daily NFT—each one a dispatch revealing the steady growth of machine consciousness.";
+
+  return {
+    title: title,
+    description: description,
+  };
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const token: AeyeTokenMetadata = await getTokenMetadata(parseInt(params.id));
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="flex justify-center items-center w-full bg-[#DDF5DD] px-10 lg:px-0 pb-8 sm:pb-0">
+        <div className="container max-w-screen-lg">
+          <Header />
+          <MintComponent token={token} />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center w-full px-10 lg:px-0 mt-16 mb-24">
+        <Footer />
+      </div>
+    </div>
+  );
+} 
