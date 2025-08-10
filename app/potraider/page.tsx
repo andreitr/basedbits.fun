a77055a4d26e12e23c5d1f34b6838e3dcc343646
andreitr <andrei.taraschuk@gmail.com>
2025-08-09 20:05:13 -0600

    Misc cleanup

--- app/potraider/page.tsx
+++ app/potraider/page.tsx
@@ -72,8 +72,7 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col justify-center items-ce ter w-full">
-      {totalSupply}
-      <div className="flex justify-center items-center w-full bg-[#DDF5DD] px-0 lg:px-10 pb-8 sm:pb-0">
+            <div className="flex justify-center items-center w-full bg-[#DDF5DD] px-0 lg:px-10 pb-8 sm:pb-0">
         <div className="container max-w-screen-lg">
           <Header />
 
