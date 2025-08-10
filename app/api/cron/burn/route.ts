5925c56ad70dd66a8592d985bbb7c7582373e465
andrei <andrei.taraschuk@gmail.com>
2025-08-09 19:45:53 -0600

    Reduce burn amount

--- app/api/cron/burn/route.ts
+++ app/api/cron/burn/route.ts
@@ -26,8 +26,8 @@ export async function GET(req: NextRequest) {
     );
 
     const signer = new Wallet(process.env.BURNER_BOT_PK as string, provider);
-    const burnAmount = parseUnits("0.0004", 18);
-    const minAmount = parseUnits("0.00045", 18);
+    const burnAmount = parseUnits("0.0002", 18);
+    const minAmount = parseUnits("0.00025", 18);
 
     // Check wallet balance before proceeding
     const ethBalance = await provider.getBalance(signer.address);
