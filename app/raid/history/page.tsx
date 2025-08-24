d76b7ea6efb0aa33bc845b6183d6d6d198b0c223
andreitr <andrei.taraschuk@gmail.com>
2025-08-24 14:19:45 -0600

    History placeholder

---
+++ app/raid/history/page.tsx
@@ -0,0 +1,73 @@
+"use server";
+
+import { Header } from "@/app/lib/components/client/Header";
+import { Footer } from "@/app/lib/components/Footer";
+import { potraiderContract } from "@/app/lib/contracts/potraider";
+import { MintComponent } from "@/app/raid/components/MintComponent";
+import { NFTList } from "@/app/raid/components/NFTList";
+import { UserComponent } from "@/app/raid/components/UserComponent";
+import { formatUnits } from "ethers";
+
+export async function generateMetadata() {
+  const contract = potraiderContract();
+  const jackpot = await contract.getLotteryJackpot();
+  const jackpotFormatted = Number(formatUnits(jackpot, 6)).toLocaleString(
+    "en-US",
+    {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    },
+  );
+
+  const title = "Pot Raiders";
+  const description = `For a full year, Pot Raiders will spend a share of the treasury on Megapot tickets. Current jackpot: $${jackpotFormatted}`;
+  const ogPreviewPath = `${process.env.NEXT_PUBLIC_URL}/api/images/raid`;
+
+  return {
+    title: title,
+    description: description,
+
+    openGraph: {
+      images: [
+        {
+          url: ogPreviewPath,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title,
+      description,
+    },
+  };
+}
+
+export default async function Page() {
+  const contract = potraiderContract();
+
+  const currentDay = await contract.currentLotteryDay();
+
+  const history = await contract.lotteryPurchaseHistory(
+    currentDay > 0 ? Number(currentDay) : 0,
+  );
+
+  return (
+    <div className="flex flex-col justify-center items-ce ter w-full">
+      <div className="flex justify-center items-center w-full bg-[#DDF5DD] px-0 lg:px-10 pb-8 sm:pb-0">
+        <div className="container max-w-screen-lg">
+          <Header />
+
+          <div className="flex flex-col gap-4">
+            Purchase History for Day {currentDay}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center w-full px-10 lg:px-0 mt-16 mb-24">
+        <Footer />
+      </div>
+    </div>
+  );
+}
