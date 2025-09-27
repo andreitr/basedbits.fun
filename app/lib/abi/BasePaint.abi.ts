3192490f0d4bb5c3ceb26e7ed2cf79ce83778558
andrei <andrei.taraschuk@gmail.com>
2025-09-27 14:49:44 -0600

    Add mint function to BasePaint ABI

--- app/lib/abi/BasePaint.abi.ts
+++ app/lib/abi/BasePaint.abi.ts
@@ -1,5 +1,15 @@
 export const BasePaintAbi = [
   {
+    inputs: [
+      { internalType: "uint256", name: "day", type: "uint256" },
+      { internalType: "uint256", name: "count", type: "uint256" },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "today",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
